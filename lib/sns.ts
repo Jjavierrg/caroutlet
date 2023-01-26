f65ede1aec1f1853d0610bd04fa495a9a01c751f
Jose Javier Rodriguez Gallego <jjavierrg@gmail.com>
2023-01-26 12:20:47 +0100

    change cfnparamter type

--- lib/sns.ts
+++ lib/sns.ts
@@ -11,13 +11,13 @@ export class coSns extends Construct {
 
     const emails = new CfnParameter(this, 'notification-emails', {
       description: 'Email addresses to send notifications to (comma separated)',
-      type: 'CommaDelimitedList',
-    }).valueAsList;
+      type: 'String',
+    });
 
     this.topic = new Topic(this, 'car-outlet-newCars', { displayName: 'Car outlet new cars topic' });
 
-    if (emails?.length) {
-      emails.forEach((_, idx) => this.topic.addSubscription(new EmailSubscription(Fn.select(idx, emails))));
-    }
+    emails.valueAsString.split(',').forEach((email) => {
+      this.topic.addSubscription(new EmailSubscription(email.trim()));
+    });
   }
 }
