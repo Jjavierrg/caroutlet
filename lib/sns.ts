3c11dfaea96e2c70761c6585cf65a6ca9cd6e4f3
Jose Javier Rodriguez Gallego <jjavierrg@gmail.com>
2023-01-26 12:29:00 +0100

    move from cfn parameter to env variable

--- lib/sns.ts
+++ lib/sns.ts
@@ -1,4 +1,3 @@
-import { CfnParameter, Fn } from 'aws-cdk-lib';
 import { ITopic, Topic } from 'aws-cdk-lib/aws-sns';
 import { EmailSubscription } from 'aws-cdk-lib/aws-sns-subscriptions';
 import { Construct } from 'constructs';
@@ -9,15 +8,9 @@ export class coSns extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
 
-    const emails = new CfnParameter(this, 'notification-emails', {
-      description: 'Email addresses to send notifications to (comma separated)',
-      type: 'String',
-    });
-
     this.topic = new Topic(this, 'car-outlet-newCars', { displayName: 'Car outlet new cars topic' });
 
-    emails.valueAsString.split(',').forEach((email) => {
-      this.topic.addSubscription(new EmailSubscription(email.trim()));
-    });
+    const emails = process.env.EMAILS?.split(',') || [];
+    emails.forEach((email) => this.topic.addSubscription(new EmailSubscription(email.trim())));
   }
 }
